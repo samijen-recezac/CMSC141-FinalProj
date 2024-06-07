1acc565f0e8aecd05c1c73b1e0d31f8810837605
samijen-recezac <jasmine.cacerez@gmail.com>
2024-06-07 18:00:56 +0800

    deleted scene2 bc it has same contents as scene1

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Scene1");
+        SceneManager.LoadScene("Scene 1");
     }
 
     public void QuitGame()
